e1c4ff30fc0345d38c00c55b0e0624eeb8ac82c7
Dennis Müller <dennis.mueller@elvah.de>
2025-06-30 13:22:02 +0200

    Add originalPrice and type to ChargeOffer

--- Sources/ElvahCharge/Core/Models/ChargeOffer.swift
+++ Sources/ElvahCharge/Core/Models/ChargeOffer.swift
@@ -11,6 +11,8 @@ package struct ChargeOffer: Codable, Hashable, Identifiable, Sendable {
 
 	package var chargePoint: ChargePoint
 	package var price: ChargePrice
+	package var originalPrice: ChargePrice?
+	package var type: ChargeOffer.OfferType
 	package var campaignEndDate: Date
 	package var expiresAt: Date
 	package var signedOffer: String
@@ -19,6 +21,8 @@ package struct ChargeOffer: Codable, Hashable, Identifiable, Sendable {
 		id: String,
 		chargePoint: ChargePoint,
 		price: ChargePrice,
+		originalPrice: ChargePrice?,
+		type: ChargeOffer.OfferType,
 		campaignEndDate: Date,
 		expiresAt: Date,
 		signedOffer: String
@@ -26,6 +30,8 @@ package struct ChargeOffer: Codable, Hashable, Identifiable, Sendable {
 		self.id = id
 		self.chargePoint = chargePoint
 		self.price = price
+		self.originalPrice = originalPrice
+		self.type = type
 		self.campaignEndDate = campaignEndDate
 		self.expiresAt = expiresAt
 		self.signedOffer = signedOffer
@@ -38,11 +44,20 @@ package struct ChargeOffer: Codable, Hashable, Identifiable, Sendable {
 }
 
 package extension ChargeOffer {
+	enum OfferType: String, Codable, Hashable, Sendable {
+		case standard = "STANDARD"
+		case campaign = "CAMPAIGN"
+	}
+}
+
+package extension ChargeOffer {
 	static var mockAvailable: ChargeOffer {
 		ChargeOffer(
 			id: "mock deal available",
 			chargePoint: .mockAvailable,
 			price: ChargePrice.mock,
+			originalPrice: nil,
+			type: .campaign,
 			campaignEndDate: Date().addingTimeInterval(20),
 			expiresAt: Date().addingTimeInterval(120),
 			signedOffer: "mock deal"
@@ -54,6 +69,8 @@ package extension ChargeOffer {
 			id: "mock deal unavailable",
 			chargePoint: .mockUnavailable,
 			price: ChargePrice.mock,
+			originalPrice: nil,
+			type: .campaign,
 			campaignEndDate: Date().addingTimeInterval(-10),
 			expiresAt: Date().addingTimeInterval(120),
 			signedOffer: "mock deal"
@@ -65,6 +82,8 @@ package extension ChargeOffer {
 			id: "mock deal out of service",
 			chargePoint: .mockOutOfService,
 			price: ChargePrice.mock,
+			originalPrice: nil,
+			type: .campaign,
 			campaignEndDate: Date().addingTimeInterval(30),
 			expiresAt: Date().addingTimeInterval(120),
 			signedOffer: "mock deal"
